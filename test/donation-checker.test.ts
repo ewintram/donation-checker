d1c619d1e207a37ba7dd180e1f43d191c480033d
Eleanor Wintram <eleanor.wintram@mvfglobal.com>
2023-02-19 12:34:45 +0000

    Add cluster to stack

--- test/donation-checker.test.ts
+++ test/donation-checker.test.ts
@@ -1,17 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as DonationChecker from '../lib/donation-checker-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as DonationChecker from '../lib/donation-checker-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/donation-checker-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new DonationChecker.DonationCheckerStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('ecs cluster created', () => {
+  const app = new cdk.App();
+  const stack = new DonationChecker.DonationCheckerStack(app, 'TestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.resourceCountIs('AWS::ECS::Cluster', 1);
 });
