b344b9ab491b99b4c125ba2a4485fca4ff771141
Eleanor Wintram <eleanor.wintram@mvfglobal.com>
2023-02-21 19:23:49 +0000

    Generate new image on each deploy

--- lib/donation-checker-stack.ts
+++ lib/donation-checker-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from "aws-cdk-lib";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecrAssets from "aws-cdk-lib/aws-ecr-assets";
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
 import { Construct } from "constructs";
+import path = require("path");
 
 export class DonationCheckerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,13 +12,19 @@ export class DonationCheckerStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, "ProductionVpc");
     const cluster = new ecs.Cluster(this, "ProductionCluster", { vpc });
+
+    const asset = new ecrAssets.DockerImageAsset(this, "DonationCheckerImage", {
+      directory: path.join(__dirname, "../app/build"),
+      extraHash: `${Date.now()}`
+    });
+
     new ecsPatterns.ApplicationLoadBalancedFargateService(
       this,
       "DonationCheckerService",
       {
         cluster,
         taskImageOptions: {
-          image: ecs.ContainerImage.fromAsset("./app/build"),
+          image: ecs.ContainerImage.fromDockerImageAsset(asset),
         },
       }
     );
